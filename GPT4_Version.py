ffa6209352885a5893c23dac297a833a9aa001e5
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-16 10:22:44 +0700

    Create GPT4_Version.py

---
+++ GPT4_Version.py
@@ -0,0 +1,9 @@
+scores = {
+    "Foundation English": 80,
+    "General Business": 89,
+    "Introduction to Computer Systems": 90,
+    "Computer Programming": 91
+}
+print("********** Your Scores **********")
+for subject, score in scores.items():
+    print(f"{subject}: {score}")
